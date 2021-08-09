5f8b1a53fff1ca31fd89ff8b99014ef7879ba597
Mikhail Seregin <mikhail.a.seregin@gmail.com>
2021-08-09 11:52:58 +0300

    add DataCall Add Network Status

--- Sources/Networking/Network/WebRepository.swift
+++ Sources/Networking/Network/WebRepository.swift
@@ -13,6 +13,21 @@ public protocol WebRepository {
     var session: URLSession { get }
     var baseURL: String { get }
     var queue: DispatchQueue { get }
+    func call<Value, E>(
+        endpoint: APICall,
+        httpCodes: HTTPCodes,
+        decoder: JSONDecoder,
+        errorType: E.Type
+    ) -> AnyPublisher<Value, Error>
+        where Value: Decodable, E: Decodable
+    
+    func callData<E>(
+        endpoint: APICall,
+        httpCodes: HTTPCodes,
+        decoder: JSONDecoder,
+        errorType: E.Type
+    ) -> AnyPublisher<Data, Error>
+        where E: Decodable
 }
 
 @available(OSX 10.15, *)
@@ -39,6 +54,26 @@ extension WebRepository {
                 .eraseToAnyPublisher()
         }
     }
+    
+    func callData<E>(
+        endpoint: APICall,
+        httpCodes: HTTPCodes,
+        decoder: JSONDecoder,
+        errorType: E.Type
+    ) -> AnyPublisher<Data, Error>
+    where E: Decodable {
+        do {
+            let request = try endpoint.urlRequest(baseURL: baseURL)
+            return session
+                .dataTaskPublisher(for: request)
+                .subscribe(on: queue)
+                .tryMap { $0.data }
+                .eraseToAnyPublisher()
+        } catch let error {
+            return Fail<Data, Error>(error: error)
+                .eraseToAnyPublisher()
+        }
+    }
 }
 
 @available(OSX 10.15, *)
@@ -59,10 +94,6 @@ private extension Publisher where Output == URLSession.DataTaskPublisher.Output 
                 throw APIError.unexpectedResponse
             }
             guard httpCodes.contains(code) else {
-//                guard let description = ($0.1 as? HTTPURLResponse)?.description else {
-//                    throw APIError.httpCode(code)
-//                }
-                
                 let error = try decoder.decode(E.self, from: $0.0)
                 throw APIError.decodable(error)
             }
